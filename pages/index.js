dca98cb2880fe9064f329f021cc72cc63807bfef
argonavt11 <argonavt.al@yandex.ru>
2019-01-11 16:45:55 +0300

    add effect for sun

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import * as THREE from "three";
 import {
-  BlendFunction,
-  BloomEffect,
+  KernelSize,
+  GodRaysEffect,
   EffectComposer,
   EffectPass,
   RenderPass
@@ -20,15 +20,14 @@ export default class App extends React.Component {
       1000
     );
 
-    const effectBloom = new BloomEffect({
-      blendFunction: BlendFunction.SCREEN,
-      resolutionScale: 0.05,
-      distinction: 1
-    });
-    effectBloom.blendMode.opacity.value = 4;
+    // const effectBloom = new BloomEffect({
+    //   blendFunction: BlendFunction.SCREEN,
+    //   resolutionScale: 1,
+    //   distinction: 1
+    // });
+    // effectBloom.blendMode.opacity.value = 4;
 
-    const effectPass = new EffectPass(camera, effectBloom);
-    effectPass.renderToScreen = true;
+    
 
     var renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
@@ -37,12 +36,7 @@ export default class App extends React.Component {
     const renderScene = new RenderPass(scene, camera);
     const composer = new EffectComposer(renderer);
     composer.addPass(renderScene);
-    composer.addPass(effectPass);
-
-    const geometry = new THREE.SphereGeometry(1, 100, 100);
-    const material = new THREE.MeshBasicMaterial({ color: 0xffffff });
-    const mesh = new THREE.Mesh(geometry, material);
-    scene.add(mesh);
+    
 
     camera.position.z = 5;
 
@@ -50,14 +44,32 @@ export default class App extends React.Component {
     loader.load("/static/texture/green.jpg", texture => {
       const geometry = new THREE.SphereGeometry(1, 100, 100);
       const material = new THREE.MeshBasicMaterial({ map: texture });
-      const mesh = new THREE.Mesh(geometry, material);
-      scene.add(mesh);
+      this.mesh = new THREE.Mesh(geometry, material);
+      scene.add(this.mesh);
+
+      const godRaysEffect = new GodRaysEffect(scene, camera, this.mesh, {
+        resolutionScale: 0.75,
+        kernelSize: KernelSize.SMALL,
+        density: 0.96,
+        decay: 0.90,
+        weight: 0.4,
+        exposure: 0.55,
+        samples: 60,
+        clampMax: 1.0
+      });
+
+      godRaysEffect.dithering = true;
+
+      const effectPass = new EffectPass(camera, godRaysEffect);
+      effectPass.renderToScreen = true;
+      composer.addPass(effectPass);
       animate();
     });
 
-    var animate = function() {
+    var animate = () => {
       requestAnimationFrame(animate);
-
+      this.mesh.rotation.x += 0.01;
+			this.mesh.rotation.y += 0.01;
       composer.render();
     };
   }
