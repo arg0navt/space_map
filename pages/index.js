347f6c0c69800d258853f40f21f24327b7fa54d3
argonavt11 <argonavt.al@yandex.ru>
2019-01-22 09:05:50 +0300

    clear

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,9 @@
 import React from "react";
 import * as THREE from "three";
-import {
-  KernelSize,
-  GodRaysEffect,
-  EffectComposer,
-  EffectPass,
-  RenderPass
-} from "postprocessing";
 import Sun from "../objects/Sun";
 import Planet from "../objects/Planet";
-import Stars from "../objects/Stars";
 
-var EquirectangularToCubemap = require("three.equirectangular-to-cubemap");
 var OrbitControls = require("three-orbit-controls")(THREE);
-
 export default class App extends React.Component {
   componentDidMount() {
     this.scenes = {
@@ -30,7 +20,8 @@ export default class App extends React.Component {
     camera.position.z = 1000;
     camera.position.y = 200;
     camera.updateProjectionMatrix();
-    const controls = new OrbitControls(camera);
+    new OrbitControls(camera);
+
     // const gridHelper = new THREE.GridHelper(1000, 10);
     // console.log(gridHelper);
     // this.scenes.help.add(gridHelper);
@@ -47,13 +38,14 @@ export default class App extends React.Component {
     light.castShadow = true; // default false
     this.scenes.main.add(light);
 
-    const otherLigh = [];
+    const starsLight = [];
 
     for (let i = 0; i <= 5; i++) {
       const l = new THREE.PointLight(0xeedca5, 1, 50000);
       l.castShadow = true;
-      otherLigh.push(new THREE.PointLight(0xeedca5, 1, 50000));
+      starsLight.push(new THREE.PointLight(0xeedca5, 1, 50000));
     }
+
     new THREE.CubeTextureLoader()
       .setPath("/static/texture/")
       .load(
@@ -67,57 +59,39 @@ export default class App extends React.Component {
         ],
         texture => {
           this.scenes.main.background = texture;
-          // // this.scenes.main.backgroundSphere = true;
-          console.log(texture);
           animate();
         }
       );
 
-    otherLigh[0].position.set(0, 45000, 0);
-    otherLigh[1].position.set(0, -45000, 0);
-    otherLigh[2].position.set(0, 0, 45000);
-    otherLigh[3].position.set(0, 0, -45000);
-    otherLigh[4].position.set(45000, 0, 0);
-    otherLigh[5].position.set(-45000, 0, 0);
-
-    otherLigh.map(item => this.scenes.main.add(item));
-
-    // var helper1 = new THREE.CameraHelper(otherLigh[0]);
-    // this.scenes.main.add(helper1);
-
-    // var helper2 = new THREE.CameraHelper(otherLigh[1]);
-    // this.scenes.main.add(helper2);
-
-    //Set up shadow properties for the light
+    starsLight[0].position.set(0, 45000, 0);
+    starsLight[1].position.set(0, -45000, 0);
+    starsLight[2].position.set(0, 0, 45000);
+    starsLight[3].position.set(0, 0, -45000);
+    starsLight[4].position.set(45000, 0, 0);
+    starsLight[5].position.set(-45000, 0, 0);
+    starsLight.map(item => this.scenes.main.add(item));
 
     const sun = new Sun(this.scenes.main, camera, renderer);
 
-    const mercury = null;
-    const venus = null;
-    // const earn = null;
-    const mars = null;
-    const saturn = null;
-    const jupiter = null;
+    const mercury = new Planet({
+      scene: this.scenes.main,
+      orbitRadius: 50,
+      sizePlanet: 1,
+      intensive: 400,
+      startPositionX: 50,
+      startPositionZ: 0,
+      textureUrl: "/static/texture/mercury.jpg"
+    });
 
-    // const mercury = new Planet({
-    //   scene: this.scenes.main,
-    //   orbitRadius: 50,
-    //   sizePlanet: 1,
-    //   intensive: 400,
-    //   startPositionX: 50,
-    //   startPositionZ: 0,
-    //   textureUrl: "/static/texture/mercury.jpg"
-    // });
-
-    // const venus = new Planet({
-    //   scene: this.scenes.main,
-    //   orbitRadius: 100,
-    //   sizePlanet: 1,
-    //   intensive: 500,
-    //   startPositionX: 100,
-    //   startPositionZ: 0,
-    //   textureUrl: "/static/texture/8k_venus_atmosphere.jpg"
-    // });
+    const venus = new Planet({
+      scene: this.scenes.main,
+      orbitRadius: 100,
+      sizePlanet: 1,
+      intensive: 500,
+      startPositionX: 100,
+      startPositionZ: 0,
+      textureUrl: "/static/texture/8k_venus_atmosphere.jpg"
+    });
 
     const earn = new Planet({
       scene: this.scenes.main,
@@ -137,81 +111,80 @@ export default class App extends React.Component {
       size: 0.27
     })
 
-    // const mars = new Planet({
-    //   scene: this.scenes.main,
-    //   orbitRadius: 250,
-    //   sizePlanet: 1,
-    //   intensive: 700,
-    //   startPositionX: 0,
-    //   startPositionZ: 250,
-    //   textureUrl: "/static/texture/8k_mars.jpg"
-    // });
+    const mars = new Planet({
+      scene: this.scenes.main,
+      orbitRadius: 250,
+      sizePlanet: 1,
+      intensive: 700,
+      startPositionX: 0,
+      startPositionZ: 250,
+      textureUrl: "/static/texture/8k_mars.jpg"
+    });
 
-    // const jupiter = new Planet({
-    //   scene: this.scenes.main,
-    //   orbitRadius: 400,
-    //   sizePlanet: 4,
-    //   intensive: 800,
-    //   startPositionX: 0,
-    //   startPositionZ: 400,
-    //   textureUrl: "/static/texture/8k_jupiter.jpg"
-    // });
+    const jupiter = new Planet({
+      scene: this.scenes.main,
+      orbitRadius: 400,
+      sizePlanet: 4,
+      intensive: 800,
+      startPositionX: 0,
+      startPositionZ: 400,
+      textureUrl: "/static/texture/8k_jupiter.jpg"
+    });
 
-    // const saturn = new Planet({
-    //   scene: this.scenes.main,
-    //   orbitRadius: 650,
-    //   sizePlanet: 4,
-    //   intensive: 1200,
-    //   startPositionX: 0,
-    //   startPositionZ: 650,
-    //   textureUrl: "/static/texture/8k_saturn.jpg"
-    // });
+    const saturn = new Planet({
+      scene: this.scenes.main,
+      orbitRadius: 650,
+      sizePlanet: 4,
+      intensive: 1200,
+      startPositionX: 0,
+      startPositionZ: 650,
+      textureUrl: "/static/texture/8k_saturn.jpg"
+    });
 
-    // saturn.createRings({
-    //   textureUrl: "/static/texture/8k_saturn_ring_alpha.png",
-    //   minRadius: 5,
-    //   maxRadius: 9,
-    //   segment: 64,
-    //   lookAt: {
-    //     x: 0,
-    //     y: 90,
-    //     z: 0
-    //   }
-    // })
-
-    // new Stars(this.scenes.main, camera, sun.composer);
+    saturn.createRings({
+      textureUrl: "/static/texture/8k_saturn_ring_alpha.png",
+      minRadius: 5,
+      maxRadius: 9,
+      segment: 64,
+      lookAt: {
+        x: 0,
+        y: 90,
+        z: 0
+      }
+    })
 
     const animate = () => {
       requestAnimationFrame(animate);
+      const r = (k) => ((k * Math.PI) / 180) % 360
+
       if (sun && sun.mesh) {
         sun.mesh.rotation.y += 0.01;
       }
       if (mercury && mercury.mesh) {
-        mercury.group.rotation.y += ((0.001 * Math.PI) / 180) % 360;
+        mercury.group.rotation.y += r(0.001);
         mercury.mesh.rotation.y += 0.01;
       }
       if (venus && venus.mesh) {
-        venus.group.rotation.y -= ((0.001 * Math.PI) / 180) % 360;
+        venus.group.rotation.y -= r(0.001);
         venus.mesh.rotation.y += 0.01;
       }
       if (earn && earn.mesh) {
-        earn.group.rotation.y -= ((0.001 * Math.PI) / 180) % 360;
+        earn.group.rotation.y -= r(0.001);
         earn.mesh.rotation.y += 0.01;
-        console.log(earn["satellite_moon"]);
         if (earn.moon) {
           earn.moon.rotation.y -= ((0.1 * Math.PI) / 180) % 360;
         }
       }
       if (mars && mars.mesh) {
-        mars.group.rotation.y -= ((0.001 * Math.PI) / 180) % 360;
+        mars.group.rotation.y -= r(0.001);
         mars.mesh.rotation.y += 0.01;
       }
       if (jupiter && jupiter.mesh) {
-        jupiter.group.rotation.y -= ((0.001 * Math.PI) / 180) % 360;
+        jupiter.group.rotation.y -= r(0.001);
         jupiter.mesh.rotation.y += 0.01;
       }
       if (saturn && saturn.mesh) {
-        saturn.group.rotation.y -= ((0.001 * Math.PI) / 180) % 360;
+        saturn.group.rotation.y -= r(0.001);
         // saturn.mesh.rotation.y += 0.01;
       }
       sun.composer.render();
