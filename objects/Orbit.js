b13c12e42954991b3d807538c37c32d74758fb16
argonavt11 <argonavt.al@yandex.ru>
2019-01-22 16:26:52 +0300

    optimization orbit

--- objects/Orbit.js
+++ objects/Orbit.js
@@ -1,12 +1,13 @@
 import { CircleGeometry, LineBasicMaterial, Line } from "three";
 
+var geometryOrbit = new CircleGeometry(1, 1500);
+geometryOrbit.vertices.shift();
+geometryOrbit.rotateX(-Math.PI / 2);
+
 const Orbit = function(group, radius, intensive) {
-  console.log(radius);
-  var geometry = new CircleGeometry(radius, intensive);
-  geometry.vertices.shift();
-  geometry.rotateX(-Math.PI / 2);
   var material = new LineBasicMaterial({ color: 0x191919 });
-  const mesh = new Line(geometry, material);
+  const mesh = new Line(geometryOrbit, material);
+  mesh.scale.set(radius, radius, radius);
   group.add(mesh);
 };
 
